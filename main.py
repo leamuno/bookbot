5c3c48829ef85f76c61b832fa0aa97bfdd3a3873
Leandro Munoz <Leandro.MunozJr@gmail.com>
2024-03-26 04:22:29 +0900

    Adds a character counter

--- main.py
+++ main.py
@@ -3,10 +3,22 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
+    chars_dict = get_chars_dict(text)
+    print(chars_dict)
 
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_chars_dict(text):
+    text = text.lower()
+    chars = {}
+    for char in text:
+        if char in chars:
+            chars[char] += 1
+        else:
+            chars[char] = 1
+    return chars
 
 def get_book_text(path):
     with open(path) as f:
